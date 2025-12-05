38b5d8583d1814700f21cc9f2fac06db4e92ccdd
Lick Quid Cat <liquidcat@unknown>
2025-12-05 08:15:20 +0000

    2025, Day 5 in bash

--- 2025/05/sol1.sh
+++ 2025/05/sol1.sh
@@ -6,25 +6,25 @@ rm -rf tmp
 mkdir tmp
 curmax=0
 count=0
-total=0
 prev=0
 while IFS=- read MIN MAX; do
-	echo "  $MIN $MAX (prev=$prev curmax=$curmax)"
+	#echo "  $MIN $MAX (prev=$prev curmax=$curmax)"
 	if [ -z "$MIN" ]; then
 		continue
 	elif [ -z "$MAX" ]; then
 		if [ "$MIN" -le "$curmax" ]; then
-			echo "GOOD: $MIN"
+			#echo "GOOD: $MIN"
 			count=$((count+1))
 		fi
 		prev="$MIN"
-		total=$((total+1))
 	else
 		if [ "$MIN" -eq "$prev" ]; then
-			echo "EDGE: $prev"
+			#echo "EDGE: $prev"
 			count=$((count+1))
 		fi
-		curmax=$MAX
+		if [ "$MAX" -ge "$curmax" ]; then
+			curmax=$MAX
+		fi
 	fi
 done < <(sort -n "$input_filename")
-echo $count $total
+echo $count
