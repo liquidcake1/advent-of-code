ce3f7ab253ab46e4be58fc1551252a641fc08429
Lick Quid Cat <liquidcat@unknown>
2025-12-09 11:08:17 +0000

    2025, Day 9, Part 2 in Haskell
    Pay no heed to the diff

--- 2025/09/sol.hs
+++ 2025/09/sol.hs
@@ -23,6 +23,7 @@ main = do
   fh <- System.IO.openFile (head args) System.IO.ReadMode
   contents <- hGetContents fh
   print $ part1 (parse contents [])
+  print $ part2 (parse contents [])
 
 stringToInt :: String -> Maybe Int
 stringToInt str = readMaybe str
@@ -36,9 +37,49 @@ parse contents swapsRaw = let
 
 makePairs xs ys = [(x, y) | x <- xs, y <- ys]
 
-area ((x1, y1), (x2, y2)) = abs (x1 - x2 + 1) * abs (y1 - y2 + 1)
+area ((x1, y1), (x2, y2)) = (abs (x1 - x2) + 1) * (abs (y1 - y2) + 1)
 
 part1 parsed = let
   pairs = makePairs parsed parsed
   areas = map area pairs
   in maximum areas
+
+-- checkCoord x y = x /= y - 1 && x /= y + 1
+-- checkCoord2 x y = x /= y && x /= y - 1 && x /= y + 1
+-- checkValid ((x1, y1), (x2, y2)) =
+--   (checkCoord2 x1 x2) && (checkCoord y1 y2) || (checkCoord x1 x2) && (checkCoord2 y1 y2)
+-- We verified the input always jumps at least 2 in one direction.
+
+isBetween low high a = a > low && a < high
+
+doesCross low high a b = let
+  in not ((a >= high && b >= high) || (a <= low && b <= low))
+
+isPairIntersectedByAdj (rect1, rect2) (line1, line2) = let
+  (r1x, r1y) = rect1
+  (r2x, r2y) = rect2
+  (l1x, l1y) = line1
+  (l2x, l2y) = line2
+  top = min r1y r2y
+  bottom = max r1y r2y
+  left = min r1x r2x
+  right = max r1x r2x
+  in if l1x == l2x then
+    isBetween left right l1x &&
+    doesCross top bottom l1y l2y
+  else
+    isBetween top bottom l1y &&
+    doesCross left right l1x l2x
+
+isPairValid adjacents pair = and $ map not $ map (isPairIntersectedByAdj pair) adjacents
+
+part2 parsed = let
+  pairs = makePairs parsed parsed
+  adjacents = (last parsed, head parsed):(zip parsed $ tail parsed)
+  validPairs = filter (isPairValid adjacents) pairs
+  areas = map area validPairs
+  in maximum areas
+
+
+
+-- 1439862255 is too low
